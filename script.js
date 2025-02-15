e8b5756ccf1217fc8af7702edf7ee1528b6602b3
sotoprogramador <soto.programador@gmail.com>
2025-02-15 10:15:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 var nome = prompt("Digite o seu nome por favor :")
 alert("SEJA BEM VINDO/a: " + nome)
 
-document.getElementById("search-button").addEventListener
-("click", function() 
-{let searchTerm = document.getElementById("search-input").value;
+// document.getElementById("search-button").addEventListener
+// ("click", function() 
+// {let searchTerm = document.getElementById("search-input").value;
 
-    if (searchTerm.trim() !== "") {
-        alert("voce  pesquisou por:" + searchTerm);
-        //Aqui voce pode adicionar logica para buscar resultados
-    }  else {
-        alert("Digite  algo para pesquisar!");
-    }
-});
+//     if (searchTerm.trim() !== "") {
+//         alert("voce  pesquisou por:" + searchTerm);
+//         //Aqui voce pode adicionar logica para buscar resultados
+//     }  else {
+//         alert("Digite  algo para pesquisar!");
+//     }
+// });
